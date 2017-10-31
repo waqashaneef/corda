511a0d83e79c52c2e72341623731bef2b619b35a
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-10-31 11:35:23 +0000

    SerializeAsTokenContextImpl no longer depends on ServiceHub.

--- node/src/test/kotlin/net/corda/node/services/events/NodeSchedulerServiceTest.kt
+++ node/src/test/kotlin/net/corda/node/services/events/NodeSchedulerServiceTest.kt
@@ -104,6 +104,7 @@ class NodeSchedulerServiceTest : SingletonSerializeAsToken() {
             doReturn(null).whenever(it).devModeOptions
         }
         val validatedTransactions = MockTransactionStorage()
+        val attachments = MockAttachmentStorage()
         database.transaction {
             services = rigorousMock<Services>().also {
                 doReturn(configuration).whenever(it).configuration
@@ -114,10 +115,10 @@ class NodeSchedulerServiceTest : SingletonSerializeAsToken() {
                 }).whenever(it).networkMapCache
                 doReturn(myInfo).whenever(it).myInfo
                 doReturn(kms).whenever(it).keyManagementService
-                doReturn(CordappProviderImpl(CordappLoader.createWithTestPackages(listOf("net.corda.testing.contracts")), MockAttachmentStorage())).whenever(it).cordappProvider
+                doReturn(CordappProviderImpl(CordappLoader.createWithTestPackages(listOf("net.corda.testing.contracts")), attachments)).whenever(it).cordappProvider
                 doReturn(NodeVaultService(testClock, kms, validatedTransactions, database.hibernateConfig)).whenever(it).vaultService
                 doReturn(this@NodeSchedulerServiceTest).whenever(it).testReference
-
+                doReturn(attachments).whenever(it).attachments
             }
             smmExecutor = AffinityExecutor.ServiceAffinityExecutor("test", 1)
             mockSMM = StateMachineManagerImpl(services, DBCheckpointStorage(), smmExecutor, database)
